7bd573eb038de5b41b1bd7bc5b5893417085abe1
fritzprix <innocentevil0914@gmail.com>
2020-12-11 10:28:17 +0900

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Docker builder to play with Tensorflow & scikit-learn 
-> to use both tensorflow & scikit-learn 
+> for those who are learning ML with old books
+
+
+## Steps
+1. build docker image
+    ```shell
+    $ ./build.sh
+    ```
+2. run jupyter notebook
+   ```shell
+   $ ./run.sh
+   ```
+
+## local directory binding
+> there are 3 sub-directories mounted to docker for conveniance. each of them is intended for..
+- work : repository for .ipynb files
+- tf_logs : data exported from tensorflow (e.g. models, logs, etc.)
+- data : sample data for training of your own
 
 ## modules included
 * python2
--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 docker rm tf_lab > /dev/null 2>&1
-# docker run -it -p 8888:8888 -p 6006:6006 --name tf_lab --mount type=bind,source="$(pwd)/work",target=/home/work \
-#  --mount type=bind,source="$(pwd)/data",target=/home/data \
-#  --mount type=bind,source="$(pwd)/tf_logs",target=/home/tf_logs \
-#  tf_lab:0.1.0 
-
- docker run -it -p 8888:8888 -p 6006:6006 --name tff_lab --mount type=bind,source="$(pwd)/work",target=/home/work \
+docker run -it -p 8888:8888 -p 6006:6006 --name tf_lab --mount type=bind,source="$(pwd)/work",target=/home/work \
  --mount type=bind,source="$(pwd)/data",target=/home/data \
  --mount type=bind,source="$(pwd)/tf_logs",target=/home/tf_logs \
- tensorflow/tensorflow:1.15.2-gpu-py3-jupyter
+ tf_lab:0.1.0 
 
 docker rm tf_lab
